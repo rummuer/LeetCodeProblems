2b3797dc95fc2155fe857c76b7b5fedabfe57fd6
Mallikarjun Reddy <mallikarjun.reddy@mckinleyrice.co>
2022-12-12 06:28:25 +0530

    add countConsutruct DP

--- Dynamic_Programming/countConstruct/countconstruct.go
+++ Dynamic_Programming/countConstruct/countconstruct.go
@@ -14,11 +14,15 @@ func main() {
 	s := []string{"ab", "abc", "cd", "def", "abcd"}
 	s1 := []string{"bo", "rd", "ate", "t", "ska", "sk", "boar"}
 	s2 := []string{"e", "ee", "eee", "eeee", "eeee", "eeeee", "eeeeee"}
-	fmt.Println(canConstruct("abcdef", s))
-	fmt.Println(canConstruct("skateboard", s1))
-	fmt.Println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", s2))
+	memo := make(map[string]int)
+	fmt.Println(canConstruct("abcdef", s, memo))
+	fmt.Println(canConstruct("skateboard", s1, memo))
+	fmt.Println(canConstruct("eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef", s2, memo))
 }
-func canConstruct(target string, wordBank []string) int {
+func canConstruct(target string, wordBank []string, memo map[string]int) int {
+	if _, ok := memo[target]; ok {
+		return memo[target]
+	}
 	if target == "" {
 		return 1
 	}
@@ -26,9 +30,10 @@ func canConstruct(target string, wordBank []string) int {
 	for _, word := range wordBank {
 		if strings.Index(target, word) == 0 {
 			suffix := strings.TrimPrefix(target, word)
-			numWays := canConstruct(suffix, wordBank)
+			numWays := canConstruct(suffix, wordBank, memo)
 			totalCount += numWays
 		}
 	}
+	memo[target] = totalCount
 	return totalCount
 }
